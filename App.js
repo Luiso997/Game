b80345d8126742b3d92743719db7702b27e80f29
Luiso997 <56853502+Luiso997@users.noreply.github.com>
2022-03-02 13:31:46 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Board from "./components/Board";
+function App() {
+  
+  return (
+    <div className="App">
+      <h1 className="header">
+        TIC TAC TOE
+        <Board />
+      </h1>
+    </div>
+  );
+}
+
+export default App;
